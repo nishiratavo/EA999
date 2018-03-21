8ed9c64fc919f9d3508e3a97f158da6709854e0c
gustavo <gustavonishihara@hotmail.com>
2018-03-21 13:01:13 -0300

    rx_reg working

--- UART/source/Rx_reg.vhd
+++ UART/source/Rx_reg.vhd
@@ -25,9 +25,9 @@ END rx_reg;
 ARCHITECTURE rtl OF rx_reg IS
 -- Signals & Constants Declaration
 -------------------------------------------
-	SIGNAL 		shift_reg, next_shift_reg: 	std_logic_vector (9 downto 0);	 -- add one FF for start_bit
-	SIGNAL		out_reg, next_out_reg :	std_logic_vector(7 downto 0);
-   SIGNAL    count, next_count:      unsigned (3 downto 0) := to_unsigned(0,4);
+	SIGNAL 		shift_reg, next_shift_reg: 	std_logic_vector (9 downto 0) := (9 downto 0 => '0'); -- add one FF for start_bit
+	SIGNAL		out_reg, next_out_reg :	std_logic_vector(7 downto 0) := (7 downto 0 => '0');
+  SIGNAL    count, next_count:      unsigned (3 downto 0) := to_unsigned(0,4);
 
 BEGIN
 
@@ -36,40 +36,39 @@ BEGIN
   --------------------------------------------------
   output_ctrl: PROCESS(tick, serial_i, next_count, shift_reg, count, out_reg)
   BEGIN	
-	 IF (tick = '1') THEN
-			 next_shift_reg(0) <= serial_i;
-          next_shift_reg(1) <= shift_reg(0);
-          next_shift_reg(2) <= shift_reg(1);
-          next_shift_reg(3) <= shift_reg(2);
-          next_shift_reg(4) <= shift_reg(3);
-          next_shift_reg(5) <= shift_reg(4);
-          next_shift_reg(6) <= shift_reg(5);
-          next_shift_reg(7) <= shift_reg(6);
-          next_shift_reg(8) <= shift_reg(7);
-          next_shift_reg(9) <= shift_reg(8);
-			 --serial_o     <= shift_reg(9); 
-          next_count        <= count + 1;
-	 ELSE
+	   IF (tick = '1') THEN
+	     next_shift_reg(0) <= serial_i;
+       next_shift_reg(1) <= shift_reg(0);
+       next_shift_reg(2) <= shift_reg(1);
+       next_shift_reg(3) <= shift_reg(2);
+       next_shift_reg(4) <= shift_reg(3);
+       next_shift_reg(5) <= shift_reg(4);
+       next_shift_reg(6) <= shift_reg(5);
+       next_shift_reg(7) <= shift_reg(6);
+       next_shift_reg(8) <= shift_reg(7);
+       next_shift_reg(9) <= shift_reg(8);
+       next_count        <= count + 1;
+       next_out_reg      <= out_reg;
+
+     ELSIF (count = 10) THEN
+      next_out_reg <= shift_reg(8 downto 1);
+      next_count   <= to_unsigned(0,4);
+      next_shift_reg <= (9 downto 0 => '0');
+
+	   ELSE
 			 next_shift_reg(0) <= shift_reg(0);
-          next_shift_reg(1) <= shift_reg(1);
-          next_shift_reg(2) <= shift_reg(2);
-          next_shift_reg(3) <= shift_reg(3);
-          next_shift_reg(4) <= shift_reg(4);
-          next_shift_reg(5) <= shift_reg(5);
-          next_shift_reg(6) <= shift_reg(6);
-          next_shift_reg(7) <= shift_reg(7);
-          next_shift_reg(8) <= shift_reg(8);
+       next_shift_reg(1) <= shift_reg(1);
+       next_shift_reg(2) <= shift_reg(2);
+       next_shift_reg(3) <= shift_reg(3);
+       next_shift_reg(4) <= shift_reg(4);
+       next_shift_reg(5) <= shift_reg(5);
+       next_shift_reg(6) <= shift_reg(6);
+       next_shift_reg(7) <= shift_reg(7);
+       next_shift_reg(8) <= shift_reg(8);
 			 next_shift_reg(9) <= shift_reg(9);
-			 next_count <= count;
-	 END IF;
-	 
-    IF count = 10 THEN
-          next_out_reg <= shift_reg(8 downto 1);
-          next_count <= to_unsigned(0,4);
-	 ELSE
-			next_out_reg <= out_reg;
-			next_count <= count;
-    END IF;
+       next_out_reg      <= out_reg;
+			 next_count        <= count;
+	   END IF;
   END PROCESS output_ctrl; 
 
   --------------------------------------------------
